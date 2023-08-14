fe4dbefd6af691e75daf5aed194e4283f51dc689
Mirza Anzar Baig <mabaig222@gmail.com>
2023-08-14 13:33:28 +0530

    Improved shimmer effect

--- frontend/pages/components/Shimmer.tsx
+++ frontend/pages/components/Shimmer.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 const Shimmer: React.FC = () => {
   return (
     <div className="animate-pulse space-y-2">
-      <div className="h-14 bg-gray-200 rounded w-1/2"></div>
-      <div className="h-12 bg-gray-200 rounded"></div>
-      <div className="h-12 bg-gray-200 rounded w-3/4"></div>
-      <div className="h-12 bg-gray-200 rounded w-1/4"></div>
+      <div className="flex items-center justify-center">
+        <div className="h-14 w-14 bg-gray-200 rounded-lg"></div>
+      </div>
+      <div className="h-6 bg-gray-200 rounded w-5/6"></div>
+      <div className="h-6 bg-gray-200 rounded w-2/3"></div>
+      <div className="h-6 bg-gray-200 rounded w-4/5"></div>
+      <div className="h-12 bg-gray-200 rounded w-full"></div>
+      <div className="h-6 bg-gray-200 rounded-full w-1/2"></div>
     </div>
   );
 };
